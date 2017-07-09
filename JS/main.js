5a4da0aed664c3dcf02eb5e177343c23d4ac89f3
Ferran Noguera Vall <ferran.noguera@est.fib.upc.edu>
2017-07-09 22:08:31 +0200

    js fix & included jasmine

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,80 @@
+/*cada case consisteix en una columna de la fila que printeja */
+function printrow(repo){
+	var tr = document.createElement("tr");
+	for (var i = 0; i<5; ++i) {
+		switch(i){
+			case 0:
+				var td = document.createElement("td");
+				td.id = "namerepo";
+				var repositori = document.createElement("a");
+				repositori.class = "url";
+				repositori.href = "https://github.com/"+repo.owner.login+"/"+repo.name;
+				repositori.innerHTML = repo.name;
+				td.appendChild(repositori);
+				tr.appendChild(td);
+				break;
+			case 1:
+				td = document.createElement("td");
+				td.class = "number";
+				var img = document.createElement("img");
+				img.id = "star";
+				img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Octicons-star.svg/2000px-Octicons-star.svg.png";
+			 	td.appendChild(img);
+				tr.appendChild(td);
+				break;
+			case 2:
+				td = document.createElement("td");
+				td.class = "number";
+				td.innerHTML = repo.stargazers_count;
+				tr.appendChild(td);
+				break;
+			case 3:
+				td = document.createElement("td");
+				td.class = "number";
+				img = document.createElement("img");
+				img.id = "fork";
+				img.src = "http://timhettler.github.io/sassconf-2015/slides/assets/svg/fork.svg";
+			 	td.appendChild(img);
+				tr.appendChild(td);
+				break;
+			case 4:
+				td = document.createElement("td");
+				td.class = "number";
+				td.innerHTML = repo.forks_count;
+				tr.appendChild(td);
+				break;
+			default:
+				break;
+		}
+	}
+	return tr;
+}
+/* elimina l'antiga taula */
+function removeoldtable(taula){
+	while (taula.firstChild) {
+    	taula.removeChild(taula.firstChild);
+	}
+	return taula;
+}
 /* Printeja els repositoris */
 function printrepos(jsonrepos){
 	var taula = document.getElementById("taula");
-	var repo;
-	var a_inner = "";
+	taula = removeoldtable(taula);
     jsonrepos.forEach((repo)=>{
-    	//alert("<td id='namerepo'>"+"<a href='https://github.com/"+repo.owner.login+"/"+repo.name+"'>"+repo.name+"</a></td>");
-       a_inner = a_inner+"<tr>"+
-						"<td id='namerepo'>"+
-						"<a class='url' href='https://github.com/"+repo.owner.login+"/"+repo.name+"'>"+repo.name+"</a>"+
-						"</td>"+
-						"<td class='number'><img id='star' src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Octicons-star.svg/2000px-Octicons-star.svg.png'></td>"+
-						"<td class='number'>"+repo.stargazers_count+"</td>"+
-						"<td class='number'><img id='fork' src='http://timhettler.github.io/sassconf-2015/slides/assets/svg/fork.svg'></td>"+
-						"<td class='number'>"+repo.forks_count+"</td>"+
-						"</tr>";
+		taula.appendChild(printrow(repo));
+		/*considero que el de dalt es codi mes Clean, maintainable & easy­to­read
+		  pero tambe es podria realitzar de la seguent manera:
+      	  a_inner = a_inner+"<tr>"+
+			"<td id='namerepo'>"+
+			"<a class='url' href='https://github.com/"+repo.owner.login+"/"+repo.name+"'>"+repo.name+"</a>"+
+			"</td>"+
+			"<td class='number'><img id='star' src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Octicons-star.svg/2000px-Octicons-star.svg.png'></td>"+
+			"<td class='number'>"+repo.stargazers_count+"</td>"+
+			"<td class='number'><img id='fork' src='http://timhettler.github.io/sassconf-2015/slides/assets/svg/fork.svg'></td>"+
+			"<td class='number'>"+repo.forks_count+"</td>"+
+		  "</tr>";
+		  tenint en compte que el a_inner es fara despres del foreach en la taula. */
     });
-    taula.innerHTML = a_inner;
 }
 
 /* Carrega els repositoris */
---
+++ Jasmine/spec/testing.js
@@ -0,0 +1,13 @@
+describe("Mathsutils", function() {
+var a;
+beforeEach(function() {
+	calc = new Main();
+	});
+	describe("when calc is used to peform basic math operations", function(){
+	   //Spec for sum operation
+	    it("should be able to calculate sum of 3 and 5", function() {
+	    });
+	    it("should be shit", function() {
+	    });
+    });
+});
